63894eeefa9d8fa57c29dde90d69ba30850d9ff2
Kamran Khan <84835699+Kamran-Khan96@users.noreply.github.com>
2026-08-04 11:03:53 +0800

    Add files via upload

---
+++ associated_codes/Preprocessing_QC_and_supervised_annotation/resolvi_tma5_6.py
@@ -0,0 +1,50 @@
+import os
+import tempfile
+import pandas as pd
+import anndata as ad
+import numpy as np
+import scanpy as sc
+import scvi
+import torch
+
+scvi.settings.seed = 0
+
+adata = sc.read(
+    "/group/ms014/kkhan/Analysis/OVA_TMA/tma5_6_together_scVI/tma5_6_merged_tumour.h5ad"
+)
+
+if "spatial" in adata.obsm:
+    adata.obsm["X_spatial"] = adata.obsm["spatial"].copy()
+
+scvi.external.RESOLVI.setup_anndata(
+    adata,
+    labels_key="decoupler_fine",
+    layer="raw_counts",
+    batch_key="tma"
+)
+
+# 5. Train model
+supervised_resolvi = scvi.external.RESOLVI(adata, semisupervised=True)
+supervised_resolvi.train(max_epochs=200,
+			 early_stopping=True)
+
+# 6. Predictions + latent space
+adata.obsm["resolvi_celltypes"] = supervised_resolvi.predict(
+    adata, num_samples=3, soft=True
+)
+
+adata.obs["resolvi_predicted"] = (
+    adata.obsm["resolvi_celltypes"].idxmax(axis=1)
+)
+
+adata.obsm["X_resolVI"] = supervised_resolvi.get_latent_representation(adata)
+
+sc.pp.neighbors(adata, use_rep="X_resolVI")
+sc.tl.umap(adata)
+
+adata.write_h5ad(
+    "/group/ms014/kkhan/Analysis/OVA_TMA/tma5_6_together_scVI/tma5_6_merged_tum_resolvi.h5ad"
+)
+
+
+
